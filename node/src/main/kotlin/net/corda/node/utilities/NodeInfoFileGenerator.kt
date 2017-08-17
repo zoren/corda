490484db57d9bbef61e89223d7323367e18431dc
al-r3 <alberto.arri@r3.com>
2017-08-17 11:46:51 +0100

    created new file which will contain the code to read/write the node infos

---
+++ node/src/main/kotlin/net/corda/node/utilities/NodeInfoFileGenerator.kt
@@ -0,0 +1,11 @@
+package net.corda.node.utilities
+
+import net.corda.core.utilities.loggerFor
+
+object NodeInfoFileGenerator {
+    private val logger = loggerFor<NodeInfoFileGenerator>()
+
+    fun generateToDisk() {
+
+    }
+}
