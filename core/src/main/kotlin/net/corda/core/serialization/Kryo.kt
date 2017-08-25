033b4b9a3cc549a86da7210d9023f3511b33c5e8
Matthew Nesbit <matthew.nesbit@r3cev.com>
2017-08-25 08:55:34 +0100

    Put in PublicKey cache for EdDSA

--- core/src/main/kotlin/net/corda/core/serialization/Kryo.kt
+++ core/src/main/kotlin/net/corda/core/serialization/Kryo.kt
@@ -308,14 +308,23 @@ object Ed25519PrivateKeySerializer : Serializer<EdDSAPrivateKey>() {
 /** For serialising an ed25519 public key */
 @ThreadSafe
 object Ed25519PublicKeySerializer : Serializer<EdDSAPublicKey>() {
+    private val cache = object : LinkedHashMap<SecureHash, EdDSAPublicKey>(4096, 0.75f, false) {
+        override fun removeEldestEntry(eldest: MutableMap.MutableEntry<SecureHash, EdDSAPublicKey>): Boolean {
+            return size > 8192
+        }
+    }
+
     override fun write(kryo: Kryo, output: Output, obj: EdDSAPublicKey) {
         check(obj.params == Crypto.EDDSA_ED25519_SHA512.algSpec)
         output.writeBytesWithLength(obj.abyte)
+        cache[SecureHash.Companion.sha256(obj.abyte)] = obj
     }
 
     override fun read(kryo: Kryo, input: Input, type: Class<EdDSAPublicKey>): EdDSAPublicKey {
         val A = input.readBytesWithLength()
-        return EdDSAPublicKey(EdDSAPublicKeySpec(A, Crypto.EDDSA_ED25519_SHA512.algSpec as EdDSANamedCurveSpec))
+        return cache.getOrPut(SecureHash.Companion.sha256(A),
+                { EdDSAPublicKey(EdDSAPublicKeySpec(A, Crypto.EDDSA_ED25519_SHA512.algSpec as EdDSANamedCurveSpec)) } )
+
     }
 }
 
